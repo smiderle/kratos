0db73467e5f6e24c4917c19211103a5edef4a87d
smiderle <ladair.junior@ciss.com.br>
2015-05-12 22:12:12 -0300

    Concluido o historico, sincronização e edição do pedido.

--- app/src/main/java/br/com/doubletouch/vendasup/data/database/dao/OrderDAO.java
+++ app/src/main/java/br/com/doubletouch/vendasup/data/database/dao/OrderDAO.java
@@ -3,21 +3,17 @@ package br.com.doubletouch.vendasup.data.database.dao;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
-import br.com.doubletouch.vendasup.VendasUp;
 import br.com.doubletouch.vendasup.dao.SQLiteHelper;
 import br.com.doubletouch.vendasup.data.database.script.OrderDB;
 import br.com.doubletouch.vendasup.data.entity.Customer;
 import br.com.doubletouch.vendasup.data.entity.Installment;
 import br.com.doubletouch.vendasup.data.entity.Order;
 import br.com.doubletouch.vendasup.data.entity.User;
+import br.com.doubletouch.vendasup.data.entity.enumeration.Flag;
 
 /**
  * Created by LADAIR on 21/04/2015.
@@ -37,7 +33,6 @@ public class OrderDAO {
     }
 
     public Long insert(Order order) {
-
         return db.insertWithOnConflict(OrderDB.TABELA, null, getContentValues(order), SQLiteDatabase.CONFLICT_REPLACE);
     }
 
@@ -50,7 +45,7 @@ public class OrderDAO {
         db.endTransaction();
     }
 
-    public Order get(Integer id) {
+    public Order get(Long id) {
         Order order = null;
         String where = OrderDB.ID+" = ? ";
         Cursor c = db.query(OrderDB.TABELA, OrderDB.COLUNAS, where, new String[]{String.valueOf(id)}, null, null, null, null);
@@ -67,7 +62,7 @@ public class OrderDAO {
         ArrayList<Order> orders = new ArrayList<>();
 
         String where = OrderDB.IDFILIAL+" = ? ";
-        Cursor c = db.query(OrderDB.TABELA, OrderDB.COLUNAS, where, new String[]{String.valueOf(branchId)}, null, null, null, LIMIT);
+        Cursor c = db.query(OrderDB.TABELA, OrderDB.COLUNAS, where, new String[]{String.valueOf(branchId)}, null, null, OrderDB.ID.concat(" DESC "), LIMIT);
 
         if(c.moveToFirst()){
             do {
@@ -99,6 +94,48 @@ public class OrderDAO {
     }
 
 
+    public Integer max() {
+
+        Integer max = 0;
+
+        String sql = "SELECT MAX("+ OrderDB.ID +") AS max_id FROM " + OrderDB.TABELA;
+
+        Cursor cursor = db.rawQuery( sql, null );
+
+        if(cursor.moveToFirst()){
+            max = cursor.getInt( 0 );
+        }
+
+        return max;
+
+
+    }
+
+    public void updateByIdMobile( Order order) {
+
+        String sql = "UPDATE " + OrderDB.TABELA + " SET " + OrderDB.ID +" = " + order.getID()+","+ OrderDB.ID_MOBILE+" = "+ order.getID() +"," + OrderDB.SYNC_PENDENTE +" = 0 "+
+                "WHERE "+ OrderDB.ID_MOBILE +" = " + order.getIdMobile();
+
+        db.execSQL(sql);
+    }
+
+    public List<Order> getAllSyncPending( Integer branchId ){
+        ArrayList<Order> orders = new ArrayList<>();
+
+        String where = OrderDB.IDFILIAL+" = ? AND " + OrderDB.SYNC_PENDENTE +" = " +Flag.TRUE.ordinal();
+        Cursor c = db.query(OrderDB.TABELA, OrderDB.COLUNAS, where, new String[]{ String.valueOf( branchId) }, null, null, null, null);
+
+        if(c.moveToFirst()){
+            do {
+                orders.add( getByCursor(c) );
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return  orders;
+    }
+
+
 
     private Order getByCursor(Cursor c){
 
@@ -115,6 +152,9 @@ public class OrderDAO {
         int idxDescontoTotal = c.getColumnIndex(OrderDB.DESCONTO_TOTAL);
         int idxObservacao = c.getColumnIndex(OrderDB.OBSERVACAO);
         int idxFormaPagamento = c.getColumnIndex(OrderDB.FORMA_PAGAMENTO);
+        int idxSyncPending = c.getColumnIndex(OrderDB.SYNC_PENDENTE);
+        int idxIdMobile = c.getColumnIndex(OrderDB.ID_MOBILE);
+        int idxTipo = c.getColumnIndex(OrderDB.TIPO);
 
         Order order = new Order();
         order.setID( c.getLong( idxId ) );
@@ -123,14 +163,17 @@ public class OrderDAO {
         order.setCustomer( new Customer( c.getInt( idxCliente )));
         order.setInstallment( new Installment( c.getInt( idxParcelamento )));
         order.setUser(new User(c.getInt(idxUsuario))) ;
-        order.setUserID(c.getInt(idxUsuario)); ;
-        order.setIssuanceTime( new Date( c.getLong( idxDataEmissao ) ) );
+        order.setUserID(c.getInt(idxUsuario));
+        order.setIssuanceTime(c.getLong(idxDataEmissao));
         order.setNetValue( c.getDouble( idxValorLiquido ));
         order.setGrossValue( c.getDouble( idxValorBruto ));
         order.setTotalDiscount( c.getDouble( idxDescontoTotal ));
         order.setObservation( c.getString( idxObservacao ));
         order.setFormPayment( c.getInt( idxFormaPagamento ));
         order.setExcluded( c.getInt( idxExcluido ) == 1 );
+        order.setSyncPending( c.getInt( idxSyncPending ) == 1 );
+        order.setIdMobile( c.getLong( idxIdMobile ) );
+        order.setType(c.getInt(idxTipo));
 
         return order;
     }
@@ -150,18 +193,19 @@ public class OrderDAO {
         cv.put(OrderDB.DESCONTO_TOTAL, order.getTotalDiscount());
         cv.put(OrderDB.OBSERVACAO, order.getObservation());
         cv.put(OrderDB.FORMA_PAGAMENTO, order.getFormPayment());
+        cv.put(OrderDB.SYNC_PENDENTE, order.isSyncPending());
+        cv.put(OrderDB.ID_MOBILE, order.getIdMobile());
+        cv.put(OrderDB.DATA_EMISSAO, order.getIssuanceTime());
+        cv.put(OrderDB.TIPO, order.getType());
 
         if(order.getInstallment() != null) {
-            cv.put(OrderDB.IDPARCELAMENTO, order.getInstallment().getInstallmentID());
+            cv.put(OrderDB.IDPARCELAMENTO, order.getInstallment().getID());
         }
 
         if(order.getCustomer() != null) {
-            cv.put(OrderDB.IDCLIENTE, order.getCustomer().getCustomerID());
+            cv.put(OrderDB.IDCLIENTE, order.getCustomer().getID());
         }
 
-        if(order.getIssuanceTime() != null) {
-            cv.put(OrderDB.DATA_EMISSAO, order.getIssuanceTime().getTime());
-        }
 
         return cv;
     }
